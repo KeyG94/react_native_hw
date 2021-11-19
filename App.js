7ae51a2f4fd08fac4b4fe2d2b3d90ecf9f81e7ec
Glenn Key <45009520+KeyG94@users.noreply.github.com>
2021-11-19 18:40:01 +0100

    added new features for testing, includes a button, horizontal scroll, scrollview tags with attributes

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
 
 export default function App() {
+  const [pressedCount, setPressedCount] = useState(0);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+      }}
+    >
       <View style={{ width: 100, height: 100, backgroundColor: "red" }} />
-      <Text>Red/Blue</Text>
+      <Text style={{ color: "white", marginTop: 5 }}>Hei Glenn</Text>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ScrollView horizontal contentContainerStyle={styles.container}>
+          <View style={{ width: 300, height: 300, backgroundColor: "red" }} />
+          <View style={{ width: 300, height: 300, backgroundColor: "green" }} />
+          <View style={{ width: 300, height: 300, backgroundColor: "blue" }} />
+        </ScrollView>
+        <ScrollView alwaysBounceVertical alwaysBounceHorizontal>
+          <Text style={{ color: "white", fontSize: 16 }}>
+            The <Text style={{ fontWeight: "bold" }}>quick brown fox</Text>{" "}
+            jumps over the lazy dog
+          </Text>
+          <Text style={{ color: "white", fontSize: 16 }}>
+            The <Text style={{ fontWeight: "bold" }}>quick brown fox</Text>{" "}
+            jumps over the lazy dog
+          </Text>
+          <Text style={{ color: "white", fontSize: 16 }}>
+            The <Text style={{ fontWeight: "bold" }}>quick brown fox</Text>{" "}
+            jumps over the lazy dog
+          </Text>
+        </ScrollView>
+        <Button
+          title="Press me"
+          onPress={() => setPressedCount(pressedCount + 1)}
+          disabled={pressedCount >= 3}
+        />
+      </View>
       <View style={{ width: 100, height: 100, backgroundColor: "blue" }} />
     </View>
   );
@@ -14,9 +48,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "blue",
+    backgroundColor: "grey",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
